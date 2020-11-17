f26c1090868ac01f6613b9d56d0127c1d5b3acb6
Enrique <enriquefernandezchacon@gmail.com>
2020-11-17 01:40:43 +0100

    PUNTO 12
    Creados metodos set y get, tengo dudas sobre como validar los datos del
    metodo setPosicion()

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -8,4 +8,31 @@ public class Alfil {
 	private Posicion posicion;
 	
 	// METODOS
+	
+	// SET Y GET
+	
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	private void setColor(Color color) {
+		if(color.equals("NEGRO")||color.equals("BLANCO")) {
+		this.color = color;
+		}else {
+			System.out.println("VALOR NO VALIDO PARA COLOR");
+		}
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	
+	
+	
+	
 }
