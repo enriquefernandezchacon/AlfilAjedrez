04c0bd9d80a10c9add7b3151d0b5b15b6d7bbd4d
Enrique <enriquefernandezchacon@gmail.com>
2020-11-17 01:17:38 +0100

    PUNTO 9
    Creados metodos equals y Hashcode

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -88,6 +88,36 @@ public class Posicion {
 	public char getColumna() {
 		return columna;
 	}
+
+	// METODOS EQUALS Y HASHCODE
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + columna;
+		result = prime * result + fila;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		if (columna != other.columna)
+			return false;
+		if (fila != other.fila)
+			return false;
+		return true;
+	}
+	
+	
+	
 	
 	
 }
