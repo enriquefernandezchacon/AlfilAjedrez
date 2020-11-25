0818cde4f36d97eb7eaf5dc013e14bef4fc63ccc
Enrique <enriquefernandezchacon@gmail.com>
2020-11-25 13:48:43 +0100

    PUNTO 19.2
    Creada clase MainApp() y mostrarMenu()

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -1,32 +1,23 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-
 import javax.naming.OperationNotSupportedException;
 
 public class MainApp {
-
-	public static void main(String[] args) throws OperationNotSupportedException {
-		// TODO Auto-generated method stub
-		Alfil alfilBlanco;
-		
-		alfilBlanco=new Alfil(Color.BLANCO);
-		try {
-			System.out.println(alfilBlanco);
-			alfilBlanco.mover(Direccion.ARRIBA_DERECHA, 2);
-			System.out.println(alfilBlanco);
-			alfilBlanco.mover(Direccion.ARRIBA_IZQUIERDA, 5);
-			System.out.println(alfilBlanco);			
-			alfilBlanco.mover(Direccion.ABAJO_IZQUIERDA, 2);
-			System.out.println(alfilBlanco);			;
-			alfilBlanco.mover(Direccion.ABAJO_DERECHA, 5);
-			System.out.println(alfilBlanco);			
-		} catch (OperationNotSupportedException ex) {
-			System.out.println(ex.getMessage());
-		}
 	
+	private Alfil alfil; 
+
+	public static void main(String[] args)  {
+		mostrarMenu();
 	}
 
+	private static void mostrarMenu() {
+		System.out.println("MENU DE OPCIONES");
+		System.out.println("----------------\n");
+		System.out.println("1. CREAR ALFIL POR DEFECTO");
+		System.out.println("2. CREAR ALFIL INTRODUCIENDO SU COLOR");
+		System.out.println("3. CREAR ALFIL INTRODUCIENDO SU COLOR Y COLUMNA INICIAL");
+		System.out.println("4. MOVER PEON CREADO");
+		System.out.println("5. SALIR");
+	}
+	
 }
